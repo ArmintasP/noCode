e12f61aa0ea1fd8d9ac41e920bbac0da8faec170
Armintas Pakenis <75906786+ArmintasP@users.noreply.github.com>
2023-05-26 18:05:39 +0300

    Flower arrangements controller. (#5)
    * Flower arrangements controller.
    
    * Add PR changes.

---
+++ Backend/NoCode.FlowerShop.Api/Controllers/FlowerArrangementsController.cs
@@ -0,0 +1,20 @@
+﻿using MapsterMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NoCode.FlowerShop.Api.Controllers;
+
+[Route("flower-arrangements")]
+public class FlowerArrangementsController : ApiController
+{
+    private readonly IMapper _mapper;
+    private readonly ISender _mediator;
+
+    public FlowerArrangementsController(ISender mediator, IMapper mapper)
+    {
+        _mediator = mediator;
+        _mapper = mapper;
+    }
+
+}
+
