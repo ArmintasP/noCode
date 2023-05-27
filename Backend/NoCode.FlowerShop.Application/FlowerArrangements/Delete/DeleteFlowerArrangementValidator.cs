a9f7dc37c432af18425e4435657bc7fc68718268
GabijaG <gabija.gakaite@mif.stud.vu.lt>
2023-05-27 10:26:01 +0300

    PR fix
    Deleted excessive validation.

--- Backend/NoCode.FlowerShop.Application/FlowerArrangements/Delete/DeleteFlowerArrangementValidator.cs
+++ Backend/NoCode.FlowerShop.Application/FlowerArrangements/Delete/DeleteFlowerArrangementValidator.cs
@@ -3,8 +3,5 @@
 namespace NoCode.FlowerShop.Application.FlowerArrangements.Delete;
 public sealed class DeleteFlowerArrangementValidator : AbstractValidator<DeleteFlowerArrangementCommand>
 {
-    public DeleteFlowerArrangementValidator()
-    {
-        RuleFor(x => x.Id).NotEmpty();
-    }
+    public DeleteFlowerArrangementValidator() { }
 }
